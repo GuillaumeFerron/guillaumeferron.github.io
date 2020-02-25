637490e54b74e6f2859d5210c402056e72ff6cca
GuillaumeFerron <guillaume.ferron2@gmail.com>
2020-02-25 15:53:04 -0500

    Make the font cdn hosted

--- candy-contest/js/main.js
+++ candy-contest/js/main.js
@@ -9,7 +9,7 @@ const bevelSegments = 10
 const size = 100
 
 const loader = new THREE.FontLoader()
-loader.load('candy-contest/fonts/helvetiker_bold.typeface.json', _font => {
+loader.load('https://github.com/rollup/three-jsnext/blob/master/examples/fonts/helvetiker_bold.typeface.json', _font => {
   font = _font
   init()
   animate()
